54b65ffb5b2194a5a4852766f029535365009673
Megumi Kimura <kimuramegumi@gmail.com>
2023-08-08 21:18:00 +0200

    round category

--- src/pages/Home/ProductList/filters/categoryFilter/Categories.jsx
+++ src/pages/Home/ProductList/filters/categoryFilter/Categories.jsx
@@ -7,8 +7,9 @@ export default function Categories({ changeCategory, currentCat }) {
       {categories.map((cat) => (
         <button
           key={cat.category}
-          className={`cursor-pointer basis-20 flex-grow md:basis-20 xl:basis-24 outline outline-4 outline-pink rounded-2xl py-1 px-2 text-center hover:outline-blue hover:text-blue ${
-            currentCat === cat.category && "bg-pink text-slate-700 "
+          className={`cursor-pointer basis-20 flex-grow md:basis-20 xl:basis-24 outline outline-4 outline-pink rounded-xl py-1 px-2 text-center hover:outline-blue hover:text-blue ${
+            currentCat === cat.category &&
+            "bg-pink text-slate-700 hover:text-slate-700"
           }`}
           onClick={() => changeCategory(cat.category)}
         >
